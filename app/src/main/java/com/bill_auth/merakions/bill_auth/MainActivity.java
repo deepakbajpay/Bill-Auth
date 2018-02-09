337596b839ef42c974e113f2c165712836cc870e
Hardik <hardiksharmax@gmail.com>
2018-02-10 00:54:14 +0530

    initial commit

---
+++ app/src/main/java/com/bill_auth/merakions/bill_auth/MainActivity.java
@@ -0,0 +1,21 @@
+package com.bill_auth.merakions.bill_auth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    LinearLayout newBillLl,billHistoryLl,wholeSellerLl,profileLl;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        newBillLl =  findViewById(R.id.newbills);
+        billHistoryLl= findViewById(R.id.billhistory);
+        wholeSellerLl= findViewById(R.id.wholesalors);
+        profileLl= findViewById(R.id.myprofile);
+
+    }
+
+}
